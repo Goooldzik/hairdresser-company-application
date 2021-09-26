61cee95fd7790156fc05b3ae065ea1e9e2d1ecd0
Patrick <trybikpolski@gmail.com>
2021-09-26 15:53:13 +0200

    Added request for update client form

---
+++ app/Http/Requests/ClientUpdateRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ClientUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'nullable',
+            'surname' => 'nullable',
+            'phone_number' => 'nullable',
+            'email' => 'nullable',
+            'updated_at' => 'nullable'
+        ];
+    }
+}
