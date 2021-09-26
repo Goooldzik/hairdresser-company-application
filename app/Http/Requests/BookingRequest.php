e8ecadafb7e5537a38deea1ed6bf7d247dad7d20
Patrick <trybikpolski@gmail.com>
2021-09-26 15:53:50 +0200

    Added request for booking visits

---
+++ app/Http/Requests/BookingRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class BookingRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'client_id' => 'required',
+            'hairdresser_id' => 'required',
+            'visit_at' => 'required',
+            'created_at' => 'required'
+        ];
+    }
+}
