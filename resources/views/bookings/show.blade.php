42c3c676920e390e70ad43e4ead3faf4d8bc4291
Patrick <trybikpolski@gmail.com>
2021-09-26 21:30:14 +0200

    Added logic for change visit status, moved logic to model

--- resources/views/bookings/show.blade.php
+++ resources/views/bookings/show.blade.php
@@ -3,6 +3,22 @@
 @section('content')
     <div class="container">
         <div class="row">
+            <div class="col-sm-12">
+                <div class="card">
+                    <div class="card-header">
+                        {{ $booking->client->name }} {{ $booking->client->surname }} visit on {{ $booking->visit_at }}
+                    </div>
+                </div>
+            </div>
+        </div>
+        @if($booking->status == 0)
+            <div class="row py-3">
+                <div class="col-sm-3">
+                    <button class="btn btn-primary w-100" id="change-visit-status">Change visit status</button>
+                </div>
+            </div>
+        @endif
+        <div class="row">
             <div class="col-sm-8">
                 <div class="card">
                     <div class="card-header">Client information</div>
@@ -41,7 +57,7 @@
                         Last 5 visits
                     </div>
                     <div class="card-body">
-                        @foreach($booking->client->library()->limit(5)->get() as $library)
+                        @foreach($booking->client->getLastVisits() as $library)
                             <span style="float: left;">{{ $library->content }}</span>
                             <span style="float: right;">{{ $library->created_at }}</span>
                             <br />
@@ -54,4 +70,46 @@
             </div>
         </div>
     </div>
+@endsection
+
+@section('js-text')
+    <script>
+        $(function (){
+            $('#change-visit-status').click(function (){
+                Swal.fire({
+                    icon: 'warning',
+                    title: 'You want change visit status? You can do it only one time',
+                    showDenyButton: false,
+                    showCancelButton: true,
+                    confirmButtonText: 'Change',
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        $.ajax({
+                            url: '{{ route('booking.change', $booking->id) }}',
+                            type: 'PUT',
+                            success: function (response){
+                                console.log(response)
+                                Swal.fire({
+                                    position: 'top-end',
+                                    icon: 'success',
+                                    title: 'Status changed',
+                                    showConfirmButton: false,
+                                    timer: 1500
+                                })
+                            },
+                            error: function (response){
+                                Swal.fire({
+                                    position: 'top-end',
+                                    icon: 'error',
+                                    title: response.responseJSON.message,
+                                    showConfirmButton: false,
+                                    timer: 10000
+                                });
+                            }
+                        });
+                    }
+                });
+            });
+        });
+    </script>
 @endsection
